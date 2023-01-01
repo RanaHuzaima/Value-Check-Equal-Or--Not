8d8433e368d18a798bfdf17f896b85cecb98c748
Rana Huzaima <huzaimadigital@gmail.com>
2023-01-01 07:02:52 +0500

    First Commit

---
+++ demoo1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    cout<<"Enter A Number"<<endl;
+    cin>>a;
+    cout<<"Enter B Number"<<endl;
+    cin>>b;
+    if(a==b){
+        cout<<"Both value is Equal"<<endl;
+    }
+    else {
+        cout<<"Both Value is not Equal";
+    }
+    
+    return 0;
+
+}
